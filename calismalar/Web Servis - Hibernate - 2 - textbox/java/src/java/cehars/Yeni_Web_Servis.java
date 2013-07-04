6ecbf86eed043df84fda2c56ba2f763ba8d0b5f4
cehars <cehars@bil.omu.edu.tr>
2013-07-05 00:17:23 +0300

    java-csharp-mysql-hibernate

---
+++ calismalar/Web Servis - Hibernate - 2 - textbox/java/src/java/cehars/Yeni_Web_Servis.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cehars;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import org.hibernate.classic.Session;
+
+/**
+ *
+ * @author cehars
+ */
+@WebService(serviceName = "Yeni_Web_Servis")
+public class Yeni_Web_Servis {
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "operasyon1")
+    public String operasyon1() {
+        //TODO write your implementation code here:
+        return "cehars";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "operasyon2")
+    public String operasyon2(@WebParam(name = "parameter") String parameter) {
+        //TODO write your implementation code here:
+       Session sess = NewHibernateUtil.getSessionFactory().openSession();
+        Rezervasyon deneme = (Rezervasyon)sess.get(Rezervasyon.class, 1);
+        System.out.print(deneme.getIsim());
+        
+        return deneme.getIsim();
+        
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "operasyon3")
+    public String operasyon3(@WebParam(name = "perametre1") String perametre1) {
+        
+        int id = Integer.parseInt(perametre1);
+        
+        Session sess = NewHibernateUtil.getSessionFactory().openSession();
+        Rezervasyon deneme = (Rezervasyon)sess.get(Rezervasyon.class, id);
+       // System.out.print(deneme.getIsim());
+        
+        return deneme.getIsim();
+    }
+}
