28eb8bf65b342e1f02a79d54bc3d378333b04c77
cehars <cehars@bil.omu.edu.tr>
2013-07-06 23:40:05 +0300

    fiyat ve sorgu

---
+++ calismalar/kayit ve sorgu/c sharp/WindowsFormsApplication12/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Yeni_Web_ServisClient myService2 = new ServiceReference1.Yeni_Web_ServisClient();
+
+
+            MessageBox.Show(myService2.ekle(textBox1.Text, textBox2.Text).ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Yeni_Web_ServisClient myService2 = new ServiceReference1.Yeni_Web_ServisClient();
+
+            MessageBox.Show(myService2.sorgu(textBox3.Text).ToString());
+        }
+    }
+}
